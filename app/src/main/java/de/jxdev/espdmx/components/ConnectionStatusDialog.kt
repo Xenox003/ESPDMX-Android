1a720a321ffa9652126138a5a4bc58359c452ace
Xenox003 <xenox003yt@gmail.com>
2023-06-11 19:27:57 +0200

    Update Modal Size

--- app/src/main/java/de/jxdev/espdmx/components/ConnectionStatusDialog.kt
+++ app/src/main/java/de/jxdev/espdmx/components/ConnectionStatusDialog.kt
@@ -1,30 +1,73 @@
 package de.jxdev.espdmx.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ConnectionStatusDialog (setShowDialog: (Boolean) -> Unit) {
     Dialog (
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
         onDismissRequest = {
             setShowDialog(false)
         }
     ) {
         Surface (
+            modifier = Modifier
+                .fillMaxSize(0.7f),
             shape = RoundedCornerShape(5.dp),
             color = MaterialTheme.colorScheme.surfaceVariant
         ) {
-            Box (
-                contentAlignment = Alignment.Center
+            Column(
+                modifier = Modifier
+                    .padding(5.dp)
             ) {
-                Text(text = "test")
+                Row() {
+                    Text(text = "System Controls", style = MaterialTheme.typography.bodyLarge, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.weight(1f))
+                    Box(
+                        modifier = Modifier
+                            .height(25.dp)
+                            .clickable { setShowDialog(false) }
+                            .background(MaterialTheme.colorScheme.error)
+                            .aspectRatio(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "X", color = MaterialTheme.colorScheme.onError)
+                    }
+                }
+
+                Box (
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "test")
+                }
             }
         }
     }
