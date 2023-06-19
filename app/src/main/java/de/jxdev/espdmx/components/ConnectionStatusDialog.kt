14c8b899d323cfcfa8c410beeafc14dec8777ce4
Xenox003 <xenox003yt@gmail.com>
2023-06-19 18:06:47 +0200

    Add Console screen (without Scrollbar)

--- app/src/main/java/de/jxdev/espdmx/components/ConnectionStatusDialog.kt
+++ app/src/main/java/de/jxdev/espdmx/components/ConnectionStatusDialog.kt
@@ -1,53 +1,31 @@
 package de.jxdev.espdmx.components
 
-import android.util.Log
-import android.view.MotionEvent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
@@ -101,22 +79,28 @@ fun ConnectionStatusDialog (setShowDialog: (Boolean) -> Unit) {
 
 @Composable
 fun WebsocketLogWindow () {
-    /*
-    LazyColumn(
-        modifier = Modifier
-            .simpleVerticalScrollbar(rememberLazyListState(), 10.dp)
-            .fillMaxSize()
+    val listState = rememberLazyListState()
 
+    Box (
+        modifier = Modifier
+            .background(Color.Black)
+            .padding(1.dp)
     ) {
-        items(20)  {
-            WebsocketLogEntry(text = "test")
+        LazyColumn(
+            reverseLayout = true,
+            state = listState,
+            modifier = Modifier
+                .fillMaxSize()
+
+        ) {
+            items(20) {
+                WebsocketLogEntry(text = "test")
+            }
         }
     }
-    */
-
 }
 
 @Composable
 fun WebsocketLogEntry (text : String) {
-    Text(text = text)
+    Text(text = text, fontFamily = FontFamily.Monospace, fontSize = 12.sp)
 }
